ce603061ed2c2d1a0b88e16b3adf14707d2a2cfb
suhani180 <suhanisengar0108@gmail.com>
2024-09-10 00:15:45 +0530

    First Commit

---
+++ components/Dashboard.jsx
@@ -0,0 +1,6 @@
+// multiple export but one default export  iy changes import Dashboard from import {Dashboard} 
+export default function Dashboard() {
+    return <div>
+        Dashboard Page
+    </div>
+}
